6a2101ab5e95b4c2d164e6b63de82a8d2e022736
Naman Kumar <126446045+NAMan00018@users.noreply.github.com>
2025-01-01 23:49:45 +0000

    Variables Learned

---
+++ Basics/Variables.js
@@ -0,0 +1,12 @@
+const accountId = 14445
+let accountemail = "naman@google.com"
+var accountPassword = "12345"
+accountCity="delhi"
+
+// accountId=890 // can't be changed once set
+accountemail="nk@gmail.com"
+accountPassword="763760"
+accountCity="goa"
+console.log(accountId);
+
+console.table([accountId,accountPassword,accountemail,accountCity]);
