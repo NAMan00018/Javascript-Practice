f0e4d337105d6ad28589d8e898e5b1faa1b3e693
Naman Kumar <126446045+NAMan00018@users.noreply.github.com>
2025-01-01 23:49:04 +0000

    Datatypes added

---
+++ Basics/Datatypes.js
@@ -0,0 +1,23 @@
+"use strict";// treat all js code as newer version
+// alert(3+3) // alert works well in browser with this syntax but we have to define different syntax to use alert in nodejs. 
+
+
+let name="Naman"
+let age=20
+let isloggedin=false
+
+// number
+// bigint
+// string => ""
+// boolean => true/false
+// null=> standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+console.log(typeof "Naman");
+ 
+console.log(typeof null) // typeof(null) is object
+
+console.log(typeof undefined); //typeof(undefined) is undefined
